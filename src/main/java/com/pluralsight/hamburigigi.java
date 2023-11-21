0ec6e45b6da8fa129715649aeb4d6a816ea31952
JGraham-appdev <jgraham@appdev.yearup.org>
2023-11-21 09:46:52 -0500

    hamburge

---
+++ src/main/java/com/pluralsight/hamburigigi.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class hamburigigi {
+
+    private boolean toHamOrNotToHam;
+
+    public hamburigigi(boolean toHamOrNotToHam) {
+        this.toHamOrNotToHam = toHamOrNotToHam;
+    }
+
+    public boolean isToHamOrNotToHam() {
+        return toHamOrNotToHam;
+    }
+
+    public void setToHamOrNotToHam(boolean toHamOrNotToHam) {
+        this.toHamOrNotToHam = toHamOrNotToHam;
+    }
+}

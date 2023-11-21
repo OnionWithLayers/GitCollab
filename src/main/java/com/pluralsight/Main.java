a82c00ad9c4cbc0718da85c8f50ae2704b4ca483
DKim-appdev <danhkim2003@gmail.com>
2023-11-21 09:08:35 -0500

    Change the main class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,5 +3,10 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int[] array = {1, 2, 3};
+        for(int list : array){
+            System.out.println(list);
+        }
+        System.out.println("\nI made a few changes");
     }
 }

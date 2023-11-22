a746e821cf09cb9f07600c979a02ec06e592efed
DKim-appdev <danhkim2003@gmail.com>
2023-11-21 19:54:08 -0500

    some pseudoCode that's definitely wrong

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,7 +2,8 @@ package com.pluralsight;
 
 import java.util.ArrayList;
 
-public class Main {
+public class Main { //DISCLAIMER THE TSUDOCODE IS MOST DEFINITELY WRONG
+    //Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello World");
 
@@ -11,5 +12,35 @@ public class Main {
         ints.add(2);
         ints.add(0);
         ints.add(9);
+
+
+        /*
+        Sandwich sandwich = new Sandwich;
+
+        "welcome to sandwich shop"
+        sandwich = sandwich code();
+
+
+        when calculating price, make calculatingPrice method, take the 'sandwich' as an input, have the program
+        maybe section off or organize the elements in the sandwich and find the match the elements inside to what
+        prices are associated to what item
+         */
     }
+
+    /*
+    ***sandwich code(Sandwich sandwich){
+    "what size sandwich do you want? (4", 8", 12")"
+    int size = scanner.next(int);
+    scanner.nextline();
+     "What type of bread would you like?"
+     "white, wheat, rye, wrap"
+     String breadType = scanner.nextLine();
+     "Toppings?"
+     String topping = scanner.nextLine();
+     "Whats the sauce, boss"
+     String sauce = scanner.nextLine();
+
+     Sandwich sandwich = new Sandwich(size, breadType, topping, sauce);
+    }
+     */
 }
